156aab853f1a7208519011eec06e01854466da5f
Robyn Craig <robynocraig@gmail.com>
2018-07-24 20:57:29 -0500

    added functionality for the images to shuffle on click

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
 
+// this function shuffles the images from friends.json
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+  tileClick = id => {
+    // this shuffles the images
+    this.handleShuffle();
+  };
+
+  // shuffle image function
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
@@ -25,7 +38,8 @@ class App extends Component {
         <Title>{"Bob's Burgers - Click Away!"}</Title>
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            tileClick={this.tileClick}
+            handleShuffle={this.handleShuffle}
             id={friend.id}
             key={friend.id}
             name={friend.name}
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
+  <span onClick={() => props.tileClick(props.id)} className="swap">
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
     </div>
-  </div>
+  </span>
 );
 
 export default FriendCard;
